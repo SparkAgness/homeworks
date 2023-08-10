346031e0e679f9cb05d45708c2a11e7d780dc298
Spark Agness <anabol1984@yandex.ru>
2023-08-10 06:28:31 +0300

    task_1.3.py is ready

--- homeworks/lvl_1/task_1.3.py
+++ homeworks/lvl_1/task_1.3.py
@@ -15,49 +15,67 @@
 
     # Введите номер месяца: 15
     # Такого месяца нет!
+
 m_number = int(input("Введите номер месяца: "))
 match m_number:
     case 1: 
         month = "январь"
         days = 31
+        flag = 1
     case 2:
         month = "февраль"
         days = 28
+        flag = 0
     case 3:
         month = "март"
         days = 31
+        flag = 1
     case 4:
         month = "апрель"
         days = 30
+        flag = 0
     case 5:
         month = "май"
         days = 31
+        flag = 1
     case 6:
         month = "июнь"
         days = 30
+        flag = 0
     case 7:
         month = "июль"
         days = 31
+        flag = 1
     case 8:
         month = "август"
         days = 31
+        flag = 1
     case 9:
         month = "сентябрь"
         days = 30
+        flag = 0
     case 10:
         month = "октябрь"
         days = 31
+        flag = 1
     case 11:
         month = "ноябрь"
         days = 30
+        flag = 0
     case 12:
         month = "декабрь"
-        days = 30
+        days = 31
+        flag = 1
     case _:
         print('Такого месяца нет!')
 
+
 if 13 > m_number > 0:
-    print(f"Вы ввели {month}. {days} дней")
+    if flag:
+        print(f"Вы ввели {month}. {days} день")
+    if not flag:
+        print(f"Вы ввели {month}. {days} дней")
+    
 
 
 
