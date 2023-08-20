d582eb8f9955e37cd71c12e8e512eb45d7fc84e7
Andrey Fedorishin <a.p.fedorishin@yandex.ru>
2023-08-20 12:40:40 +0300

    all tasks is ready

--- homeworks/lvl_2/task_2.4.py
+++ homeworks/lvl_2/task_2.4.py
@@ -8,7 +8,14 @@
 # foo("Oh, no!!!") -> "Oh, no"
 
 def remove_exclamation_marks(s):
-    pass
+    scpy = ''
+    for c in s:
+        if '!' == c:
+            pass
+        else:
+            scpy += c
+    return scpy
+        
 
 
 # Пункт B.
@@ -18,7 +25,12 @@ def remove_exclamation_marks(s):
 # remove("!Hi") == "!Hi"
 
 def remove_last_em(s):
-    pass
+    if (s.rfind('!') == (len(s) - 1)):
+        return s[:-1]
+    else:
+        return s
+    
+
 
 
 # Дополнительно
@@ -36,4 +48,24 @@ def remove_last_em(s):
 # remove("Hi! !Hi! Hi!") === "!Hi!"
 
 def remove_word_with_one_em(s):
-    pass
+    s_list = s.split(' ') #creates list for splitted string
+    remove_list = []      #creates list for strings with one "!"
+    for i in range(len(s_list)):
+        if (s_list[i].count('!') == 1):
+            remove_list.append(s_list[i]) #appends to remove_list strings with one "!"
+    for rem in remove_list:
+        s_list.remove(rem) #removes the strings
+    if (len(s_list) == 0):
+        return ' ' #in case with empty s_list, returns empty string to cout
+    else:            
+        return ' '.join(s_list) 
+
+
+
+str_list = ['Hi!', 'Hi! Hi!','Hi! Hi! Hi!', 'Hi Hi! Hi!', 'Hi! !Hi Hi!', 'Hi! Hi!! Hi!', 'Hi! !Hi! Hi!']
+for str in str_list:
+    print(f'remove_exclamation_marks - {remove_exclamation_marks(str)}')
+for str in str_list:
+    print(f'remove_last_em - {remove_last_em(str)}')
+for str in str_list:
+    print(f'remove_word_with_one_em - {remove_word_with_one_em(str)}')
