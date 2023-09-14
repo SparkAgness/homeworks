7751655a823331d386e2d8cbf1e79ab21c8a621e
Andrey Fedorishin <a.p.fedorishin@yandex.ru>
2023-09-14 05:04:13 +0300

    All tasks are ready!

--- homeworks/lvl_3/task_3.1.py
+++ homeworks/lvl_3/task_3.1.py
@@ -26,3 +26,56 @@
 #   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
 #   - проявите фантазию :)
 
+import random
+
+class Matrixx:
+    def __init__(self, strings, rows, n, memb=None):
+        self.rows = rows
+        self.strings = strings
+        self.n = n
+        self.memb = []
+        self.help_memb = []
+        for i in range(self.strings):
+            for j in range(self.rows):
+                random.seed()
+                memb_ij = random.randint(0, self.n)
+                if 10 < memb_ij < 198:
+                    self.help_memb.append(None)
+                else:
+                    self.help_memb.append(memb_ij)
+            self.memb.append(self.help_memb)
+            self.help_memb = []
+                
+    def __repr__(self):        
+        self.main_string = ""
+        for i in range(self.strings):
+            self.help_string = ""            
+            self.help_string = '[' + ', '.join(str(el) for el in self.memb[i]) + ']' + '\n'
+            self.main_string += self.help_string
+        return self.main_string
+
+    def new_value(self, strings, rows, value):        
+        if (strings >= len(self.memb) or rows >= len(self.memb[0])):
+            print("Incorrect value of string or row of matrix")
+            return
+        self.memb[strings][rows] = value
+        return self.memb
+            
+    def change_value(self, new_value, old_value):
+        for i in range(len(self.memb)):
+            for j in range (len(self.memb[i])):
+                if self.memb[i][j] == old_value:
+                    self.memb[i][j] = new_value
+        return self.memb
+        
+    def sizes(self):
+        print(f"Strings are: {len(self.memb)}, rows are: {len(self.memb[0])}")
+
+chess_board = Matrixx(8, 8, 589)
+print(chess_board)
+chess_board.new_value(7, 7, 1)
+chess_board.new_value(0, 0, 1)
+print(chess_board)
+chess_board.change_value(13, None)
+print(chess_board)
+chess_board.sizes()
